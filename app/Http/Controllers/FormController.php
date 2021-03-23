538c9c701e93c6a245619ae96ac16e84723ac13d
Juan Torres <juanet1453@gmail.com>
2021-03-23 02:34:00 -0400

    created model/migration, api route, form controller and saving responses

---
+++ app/Http/Controllers/FormController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\ContactForm;
+
+class FormController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // request obj will have: name, email, phone
+        // optional properties: subjectInquiry, message
+
+        $validatedData = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|string',
+            'phone' => 'required|string',
+            'subjectInquiry' => 'sometimes|nullable|string',
+            'message' => 'sometimes|nullable|string',
+        ]);
+
+        $newMessage = ContactForm::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'phone' => $validatedData['phone'],
+            'subjectInquiry' => $validatedData['subjectInquiry'],
+            'message' => $validatedData['message'],
+        ]);
+
+        return response($newMessage, 201);
+
+        // return response(201)->json([
+        //     'status' => 'success',
+        //     'data' => $newMessage,
+        // ]);
+
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
